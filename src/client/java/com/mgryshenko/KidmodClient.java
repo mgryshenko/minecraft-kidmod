5fd27f8c84b454e9b674242e460751ab54d654f2
Mykhailo Gryshchenko <mgryshenko@gmail.com>
2025-04-16 14:08:14 +0300

    Initial commit

---
+++ src/client/java/com/mgryshenko/KidmodClient.java
@@ -0,0 +1,10 @@
+package com.mgryshenko;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class KidmodClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/main/java/com/mgryshenko/Kidmod.java
@@ -0,0 +1,39 @@
+package com.mgryshenko;
+
+import net.fabricmc.api.ModInitializer;
+
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.item.Items;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Kidmod implements ModInitializer {
+	// This logger is used to write text to the console and the log file.
+	// It is considered best practice to use your mod id as the logger's name.
+	// That way, it's clear which mod wrote info, warnings, and errors.
+    public static final Logger LOGGER = LoggerFactory.getLogger("kidmod");
+
+	public static final Item CUSTOM_ITEM = new CustomItem(new FabricItemSettings());
+
+
+	@Override
+	public void onInitialize() {
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
+
+		LOGGER.info("Hello Fabric world!");
+
+		Registry.register(Registries.ITEM, new Identifier("kidmod", "custom_item"), CUSTOM_ITEM);
+
+		ItemGroupEvents.modifyEntriesEvent(ItemGroups.INVENTORY).register(content -> {
+			content.addAfter(Items.MUSIC_DISC_PIGSTEP, CUSTOM_ITEM);
+		});
+	}
+}
